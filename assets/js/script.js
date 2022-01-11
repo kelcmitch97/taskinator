0c3738d934f9cab19be15dee0d28cb4e8c285a5c
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-01-10 22:29:48 -0330

    Add form submission event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
-var buttonEl = document.querySelector("#save-task"); // Use the DOM to find the element object representation of the but
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl); 
+
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
     
 
